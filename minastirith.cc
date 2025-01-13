eb697783dbfe7edbd15c22714250d2e51d7dbe0f
sossos5989 <sossos5989@naver.com>
2025-01-13 12:20:20 +0900

    20250113 minastirith 마무리

--- minastirith.cc
+++ minastirith.cc
@@ -5,29 +5,6 @@
 #define fastIO cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
-// 일단 맨 처음 큰 원을 고름.
-// 그러면 이 원과 맞닿는 원중 남은 원의 둘레를 더 많이 표현할 수 있는 원을 선택
-// 이를 반복
-
-// angle, left, right는 [0, 2]사이의 실수
-struct Circle {
-    double y, x, r, mid, left, right;
-    void compute() {
-        mid = atan2(x, y);
-        double delta = acos(1.0 - (r * r) / 128.0);
-        left = mid - delta;
-        right = mid + delta;
-        if (mid < 0) mid += 2 * M_PI;
-        if (left < 0) left += 2 * M_PI;
-        if (right < 0) right += 2 * M_PI;
-        mid /= M_PI;
-        left /= M_PI;
-        right /= M_PI;
-    }
-};
-
-bool compare(const Circle &a, const Circle &b) { return a.mid < b.mid; }
-
 int main() {
     fastIO;
     int repeat;
@@ -35,34 +12,59 @@ int main() {
     while (repeat--) {
         int n;
         cin >> n;
-        vector<Circle> list(n); // y, x, r
+        vector<pair<double, double>> arcs(n);
+        bool singleOk = false;
         for (int i = 0; i < n; i++) {
-            cin >> list[i].y >> list[i].x >> list[i].r;
-            list[i].compute();
+            double y, x, r;
+            cin >> y >> x >> r;
+            if (r >= 16.0) singleOk = true;
+            double mid = fmod(atan2(x, y) + 2.0 * M_PI, 2.0 * M_PI);
+            double delta = 2.0 * asin(r / 16.0);
+            arcs[i] = make_pair(mid - delta, mid + delta);
+        }
+        sort(arcs.begin(), arcs.end());
+
+        if (singleOk) {
+            cout << 1 << "\n";
+            continue;
         }
 
-        // 가장 큰 원을 처음으로, 중점을 시계방향으로 정렬시킵니다.
-        sort(list.begin(), list.end(), compare);
-        int cache = -1, pivot = -1;
+        int ans = 111111111;
+        bool anySuccess = false;
         for (int i = 0; i < n; i++) {
-            if (list[i].r > cache) {
-                cache = list[i].r;
-                pivot = i;
+            if (arcs[i].first <= 0 || arcs[i].second >= 2.0 * M_PI) {
+                bool localCover = true;
+                double begin = fmod(arcs[i].second, 2.0 * M_PI);
+                double end = fmod(arcs[i].first + 2.0 * M_PI, 2.0 * M_PI);
+                int used = 1, idx = 0;
+                while (begin < end) {
+                    double mx = -1;
+                    while (idx < n && arcs[idx].first <= begin) {
+                        mx = max(mx, arcs[idx].second);
+                        idx++;
+                    }
+                    if (mx <= begin) {
+                        localCover = false;
+                        break;
+                    }
+                    begin = mx;
+                    used++;
+                }
+                if (localCover) {
+                    anySuccess = true;
+                    ans = min(ans, used);
+                }
             }
         }
-        rotate(list.begin(), list.begin() + pivot, list.end());
 
-        double deltaCheck = acos(1.0 - (list[0].r * list[0].r) / 128.0);
-        if (deltaCheck >= M_PI) {
-            cout << 1 << "\n";
-            continue; // 다음 테스트케이스로
+        if (!anySuccess) {
+            cout << "IMPOSSIBLE\n";
+        } else {
+            if (ans == 111111111)
+                cout << "IMPOSSIBLE\n";
+            else
+                cout << ans << "\n";
         }
-
-        double leftPoint = list[0].left;
-        double rightPoint = list[0].right;
-
-        int used = 1;
-        bool impossible = false;
-
-        return 0;
     }
+    return 0;
+}
